fb572d7f2cf55216227b8977c0f3da995c4b0480
k00253718 <k00253718@ITSB-05.student.litdom.lit.ie>
2019-12-20 14:25:35 +0000

    Nested Control Statement Example

---
+++ src/ioi/java/ApplicationMain.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ioi.java;
+
+/**
+ *
+ * @author k00253718
+ */
+public class ApplicationMain
+{
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+	//ControlStatementExample.ifElseExample1();
+	//ControlStatementExample.ifElseExample2();
+	//ControlStatementExample.ifElseExample3A(16);
+	//ControlStatementExample.ifElseExample3A(18);
+	//ControlStatementExample.ifElseExample3B(25);
+	
+	
+	NestedControlStatementExample.ifElseIfOverkillExample1();
+	NestedControlStatementExample.ifElseIfOverkillExample2();
+	
+    }
+}
